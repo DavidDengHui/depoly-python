4d5cac60be9a29f4099a6a29e407733bd7cf6993
DavidDengHui <david.no.1@msn.cn>
2023-05-29 15:51:26 +0800

    update 1.06

--- api/index.py
+++ api/index.py
@@ -1,26 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route('/index')
+@app.route('/')
 def home():
-    return 'This is Home'
-
+    return 'Hello, World!'
 
 @app.route('/about')
 def about():
     return 'About'
-
-
-@app.route('/doit')
-def home():
-    # 创建一个json对象
-    json_data = {
-        "status": "error",
-        "code": "1001",
-        "doit": "INVALID_KEY",
-        "callback": "token"
-    }
-    # 返回json响应
-    return jsonify(json_data)
