8cf14671914ad85e559bb49b5ef5384aa7909703
DavidDengHui <david.no.1@msn.cn>
2023-05-29 18:58:11 +0800

    update 1.25

--- api/index.py
+++ api/index.py
@@ -1,12 +1,10 @@
-# 导入flask模块
 from flask import Flask, jsonify, request
-# 创建flask应用对象
+import base64
+
 app = Flask(__name__)
 
-# 定义一个路由函数，处理根路径的请求
+
 @app.route("/doit")
 def index():
-    # 获取链接中的token值
-    token = request.args.get("token")
-    # 将值输出到页面
+    token = base64.b64decode(request.args.get("token")).decode()
     return token
