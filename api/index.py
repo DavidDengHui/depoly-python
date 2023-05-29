4b160a228aabb9ff46dfb66741ec52449106b428
DavidDengHui <david.no.1@msn.cn>
2023-05-29 16:00:51 +0800

    update 1.09

--- api/index.py
+++ api/index.py
@@ -1,18 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return 'Home'
-
-
-@app.route('/about')
-def about():
-    return 'About'
-
-
 @app.route('/doit')
 def doit():
-    return 'This is API.'
+    # 创建一个json对象
+    json_data = {"status": "error","code": "1001","doit": "INVALID_KEY","callback": "token"}
+    # 创建一个响应对象
+    response = make_response(jsonify(json_data))
+    # 设置响应头
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    response.headers["Content-Type"] = "application/json; charset=utf-8"
+    # 返回响应对象
+    return response
