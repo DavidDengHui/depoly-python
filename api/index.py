7169ddd444da56dc7ef79d484d3bd89ba305c8d8
DavidDengHui <david.no.1@msn.cn>
2023-05-30 06:48:19 +0800

    update 1.32

--- api/index.py
+++ api/index.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 
 @app.route("/doit")
-def index():
+def doit():
     token = base64.b64decode(request.args.get("token")).decode()
     return jsonify({"status": "error", "code": "1001", "doit": "INVALID_KEY", "callback": token})
