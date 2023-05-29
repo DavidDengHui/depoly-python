03cb412dc15664ac812afcde44b8c9ec7bac0bc7
DavidDengHui <david.no.1@msn.cn>
2023-05-29 18:21:03 +0800

    update 1.12

--- api/index.py
+++ api/index.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify, make_response
+import requests
+import base64
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/doit')
+@app.route('/doit', methods=["GET", "POST"])
 def doit():
     # 创建一个json对象
-    json_data = {"status": "error","code": "1001","doit": "INVALID_KEY","callback": "token"}
+    json_data = {"status": "error","code": "1001","doit": "INVALID_KEY","callback": base64.b64decode("Z2hwX05VejBmZzBFSjFuVXBmSUtLU1lDZjJpN3RpdVlnZzB5VjdtRA==").decode()}
     # 创建一个响应对象
     response = make_response(jsonify(json_data))
     # 设置响应头
