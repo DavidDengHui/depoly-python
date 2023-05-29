c20ab1abae64aadb67f1df36c94e1f734fedd309
DavidDengHui <david.no.1@msn.cn>
2023-05-29 18:31:48 +0800

    update 1.13

--- api/index.py
+++ api/index.py
@@ -1,21 +1,17 @@
-from flask import Flask, jsonify, make_response
-import requests
-import base64
-import json
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route('/doit', methods=["GET", "POST"])
 def doit():
-    # 创建一个json对象
-    json_data = {"status": "error","code": "1001","doit": "INVALID_KEY","callback": base64.b64decode("Z2hwX05VejBmZzBFSjFuVXBmSUtLU1lDZjJpN3RpdVlnZzB5VjdtRA==").decode()}
-    # 创建一个响应对象
-    response = make_response(jsonify(json_data))
-    # 设置响应头
+    # 设置页面Content-Type为application/json; charset=utf-8
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "*"
     response.headers["Content-Type"] = "application/json; charset=utf-8"
-    # 返回响应对象
+    response = jsonify()
+    # 获取链接中的token值
+    token = request.args.get("token")
+    # 将值输出到页面json数据
+    response.json = {"token": token}
     return response
