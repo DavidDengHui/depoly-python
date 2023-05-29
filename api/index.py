0ac873977a7ac3183aa5ff4402dcce76b44c934b
DavidDengHui <david.no.1@msn.cn>
2023-05-29 18:49:17 +0800

    update 1.22

--- api/index.py
+++ api/index.py
@@ -1,18 +1,12 @@
-from flask import Flask, jsonify, make_response
-
+# 导入flask模块
+from flask import Flask, request
+# 创建flask应用对象
 app = Flask(__name__)
 
-
-@app.route('/doit', methods=["GET", "POST"])
-def doit():
-    # 创建一个json对象
-    json_data = {"status": "error","code": "1001","doit": "INVALID_KEY","callback": "token"}
-    # 创建一个响应对象
-    response = make_response(jsonify(json_data))
-    # 设置响应头
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "*"
-    response.headers["Content-Type"] = "application/json; charset=utf-8"
-    # 返回响应对象
-    return response
+# 定义一个路由函数，处理根路径的请求
+@app.route("/doit")
+def index():
+    # 获取链接中的token值
+    token = request.args.get("token")
+    # 将值输出到页面
+    return token
